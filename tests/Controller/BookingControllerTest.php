464a55ae8b3e76471a8f0fb3d27827f0db2cc92e
kleytondev <akatsukipb12@gmail.com>
2023-12-24 02:50:31 -0300

    test: creating integration tests - createBookingWebTestCase

---
+++ tests/Controller/BookingControllerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class BookingControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+
+    /**
+     * @throws \Exception
+     */
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+
+        static::getContainer()->get('doctrine')->getConnection()->beginTransaction();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    protected function tearDown(): void
+    {
+        static::getContainer()->get('doctrine')->getConnection()->rollBack();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCreateBooking()
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $testUser = $userRepository->findOneByEmail('akatsukipb123@gmail.com');
+
+        $this->client->loginUser($testUser);
+
+        $this->client->request(
+            'POST',
+            '/booking',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            '{"startDate": "2024-01-01", "endDate": "2024-01-05", "room": 2}'
+        );
+
+        $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        $this->assertSame('Booking complete successfully', json_decode($this->client->getResponse()->getContent(), true)['message']);
+    }
+
+}
