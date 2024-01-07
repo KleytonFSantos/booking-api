55d08c2138b1021e0d04f9d3e13639d86874371d
kleytondev <akatsukipb12@gmail.com>
2024-01-07 01:56:47 -0300

    refactor: refactoring bookingControllerTest

--- tests/Controller/BookingControllerTest.php
+++ tests/Controller/BookingControllerTest.php
@@ -3,6 +3,9 @@
 namespace App\Tests\Controller;
 
 use App\Repository\UserRepository;
+use DateInterval;
+use DateTime;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +15,7 @@ class BookingControllerTest extends WebTestCase
     private KernelBrowser $client;
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     protected function setUp(): void
     {
@@ -22,7 +25,7 @@ class BookingControllerTest extends WebTestCase
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     protected function tearDown(): void
     {
@@ -30,15 +33,11 @@ class BookingControllerTest extends WebTestCase
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function testCreateBooking()
     {
-        $userRepository = static::getContainer()->get(UserRepository::class);
-
-        $testUser = $userRepository->findOneByEmail('akatsukipb123@gmail.com');
-
-        $this->client->loginUser($testUser);
+        $this->loginUser();
 
         $this->client->request(
             'POST',
@@ -46,10 +45,31 @@ class BookingControllerTest extends WebTestCase
             [],
             [],
             ['CONTENT_TYPE' => 'application/json'],
-            '{"startDate": "2024-01-01", "endDate": "2024-01-05", "room": 2}'
+            $this->generateReservationContent()
         );
 
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
         $this->assertSame('Booking complete successfully', json_decode($this->client->getResponse()->getContent(), true)['message']);
     }
+
+    /**
+     * @throws Exception
+     */
+    protected function loginUser(): void
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('akatsukipb123@gmail.com');
+
+        $this->client->loginUser($testUser);
+    }
+
+    protected function generateReservationContent(): string
+    {
+        $date = new DateTime();
+        $startDate = $date->add(DateInterval::createFromDateString('+1 day'));
+        $endDate = $date->add(DateInterval::createFromDateString('+5 days'));
+
+        return '{"startDate":"' . $startDate->format('Y-m-d') . '", "endDate":"' . $endDate->format('Y-m-d') . '", "room": 2}';
+    }
+
 }
